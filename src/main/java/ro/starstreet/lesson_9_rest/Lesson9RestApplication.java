9290cbc63514256a0de68b672268804e8f84df86
ildar <zariil@yandex.ru>
2022-12-25 16:14:02 +0500

    Initial commit

---
+++ src/main/java/ro/starstreet/lesson_9_rest/Lesson9RestApplication.java
@@ -0,0 +1,13 @@
+package ro.starstreet.lesson_9_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson9RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson9RestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/starstreet/lesson_9_rest/Lesson9RestApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.starstreet.lesson_9_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson9RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
